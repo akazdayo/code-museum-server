61b51ad09d562a3f1e37b9a5ca7003cfd4cc2b93
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-17 17:54:55 +0900

    drizzleに移行してる途中

--- prisma/migrations/20250208094543_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `evaluation` on the `code` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "code" DROP COLUMN "evaluation",
-ADD COLUMN     "likes" INTEGER NOT NULL DEFAULT 0;
--- prisma/migrations/20250208094905_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "code" ALTER COLUMN "code" SET DATA TYPE TEXT;
