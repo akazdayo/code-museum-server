01d9da3922564ae4c1e69623d6ef698606a0e287
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-17 16:29:22 +0900

    apiの書き方を変えてみた

--- src/pages/api/vrchat/ranking.json.ts
+++ src/pages/api/vrchat/ranking.json.ts
@@ -1,12 +1,13 @@
 // ランキングにする
 // fetchの制限が5/sなので、10個くらい一つのファイルにまとめる
 export const prerender = false;
+import type { APIRoute } from "astro";
 import { GetRankings } from "../../../util/prisma";
 
-export async function GET() {
+export const GET: APIRoute = async (req) => {
 	const rankings = await GetRankings();
 	// JSON形式に変換して返す
 	return new Response(JSON.stringify(rankings), {
 		headers: { "Content-Type": "application/json" },
 	});
-}
+};
