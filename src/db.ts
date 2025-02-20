254d359bd4f876aba2f56a7df780182de60200c2
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-20 20:04:07 +0900

    環境変数直した

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
+import { config } from "dotenv";
+
+config();
 
 const sql = neon(import.meta.env.DATABASE_URL!);
 export const db = drizzle({ client: sql });
