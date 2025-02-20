ef37b0af95c4c7e1560fbe2685494d496e5c11cd
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-20 22:55:50 +0900

    途中

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,19 @@ import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
 	adapter: vercel(),
 	output: "static",
+
 	vite: {
 		plugins: [tailwindcss()],
 		ssr: {
 			external: ["path", "os", "crypto"],
 		},
 	},
+
+	integrations: [react()],
 });
