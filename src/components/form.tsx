c28f10a957c461aa1da2da63780970d589b44280
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-22 02:07:19 +0900

    フロントエンドざっと書いてみた

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,8 +2,32 @@ export default function Form() {
   return (
     <div className="flex justify-center">
       <form>
-        <textarea className="textarea" name="text"></textarea>
-        <button className="btn" type="submit">
+        <textarea defaultValue="タイトル" className="textarea" name="text"/>
+        <br/>
+        <textarea defaultValue="説明" className="textarea" name="text"/>
+        <br/>
+        <textarea defaultValue="コード" className="textarea" name="text"/>
+        <br/>
+        <textarea defaultValue="言語" className="textarea" name="text"/>
+        
+        <button className="btn" type="submit" onClick={(e) => {
+          e.preventDefault();
+          const form = e.currentTarget.closest("form");
+          if (form) {
+            const textareas = form.querySelectorAll("textarea");
+            fetch("/api/upload", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                title: textareas[0].value,
+                description: textareas[1].value,
+                code: textareas[2].value,
+                lang: textareas[3].value,
+            })
+            });}
+        }}>
           submit
         </button>
       </form>
