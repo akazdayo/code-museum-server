34166de15d1185bb7bfba603371f1142794564cf
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-02-22 23:39:13 +0900

    表示数制限を削除

--- src/pages/api/ranking.ts
+++ src/pages/api/ranking.ts
@@ -5,11 +5,7 @@ import { codeTable } from "../../schema";
 import { db } from "../../db";
 
 export async function GET() {
-	const result = await db
-		.select()
-		.from(codeTable)
-		.orderBy(codeTable.likes)
-		.limit(10);
+	const result = await db.select().from(codeTable).orderBy(codeTable.likes);
 
 	return new Response(JSON.stringify(result), {
 		status: 200,
