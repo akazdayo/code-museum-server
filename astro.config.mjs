41dc04b255559d2001bb7e58c798d5fc9f4b7b29
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-03-01 01:50:47 +0900

    cloudflare adapterに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import vercel from "@astrojs/vercel";
-
 import solidJs from "@astrojs/solid-js";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel(),
+	adapter: cloudflare(),
 	output: "static",
 
 	vite: {
